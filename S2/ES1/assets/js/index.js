ab854ab364767e99f8af5556cfce5e6ae1d35d39
DonBeppe <beppe1990a@gmail.com>
2023-04-03 15:36:06 +0200

    S2

---
+++ S2/ES1/assets/js/index.js
@@ -0,0 +1,6 @@
+document.write('<div id="container">output scritto da Javascript</div>');
+
+document.getElementById('titolo').innerHTML = 'Iniziamo la nostra pratica';
+
+alert('Caput Javascript intrat, potestas Javascript dabit');
+window.alert('Conabor studiis JavaScript');
